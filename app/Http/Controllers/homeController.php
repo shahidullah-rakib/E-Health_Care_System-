436bc13d025717aeee9078b4238ee8048dba3fb7
MsiSakin <msisakin00@gmail.com>
2021-06-14 02:00:15 +0600

    template added

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class homeController extends Controller
 {
-    
+   public function index(){
+   	return view('home.home');
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/dashboard','homeController@index');
+});*/
+//Route::get('/dashboard','homeController@index');
+Route::get('/','homeController@index');
